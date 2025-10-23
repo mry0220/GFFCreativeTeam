e03d5dc6a36e79e88c8ff4ab184baf20fde3ecc6
Okamoto5976 <kmm2559327@stu.o-hara.ac.jp>
2025-10-23 13:22:25 +0900

    DogEnemy制作プレイヤー調整
    DogEnemy制作　プレイヤーアニメーションを調整　BigEnemy制作

--- MechaAction/Assets/okamoto/Script/Enemy/DogEnemy.cs
+++ MechaAction/Assets/okamoto/Script/Enemy/DogEnemy.cs
@@ -19,8 +19,18 @@ public class DogEnemy : MonoBehaviour
     private Transform _player;
     private Rigidbody _rb;
 
+    private float _jumpPower = 7f;
+
+    private bool _isGrounded;
     private bool _moveStop = false;
     private int _direction = 0;
+
+    private bool _iswait;//waitコルーチンの重複を防ぐ
+    private bool _ismove;//moveコルーチンの重複を防ぐ
+    private bool _isattack;//attackコルーチンの重複を防ぐ
+
+    private float _fallTime;
+    private float _fallSpeed;
 
     private void Awake()
     {
@@ -35,47 +45,69 @@ public class DogEnemy : MonoBehaviour
 
     private void FixedUpdate()
     {
-
+        Vector3 velocity = _rb.velocity;
         switch (_state)
         {
             case EnemyState.Look:
                 Look();
                 if (Vector3.Distance(transform.position, _player.position) < 13f)
                 {
-                    _state = EnemyState.Move;
+                    Debug.Log("発見");
+                    _state = EnemyState.Wait;
                 }
                 break;
 
             case EnemyState.Move:
-                Move();
                 if (Vector3.Distance(transform.position, _player.position) > 17f)
                 {
                     _state = EnemyState.Look;
                 }
+                Direction();
+                if(!_ismove)//コルーチンの重複防ぐ
+                    StartCoroutine(Move());
                 break;
 
             case EnemyState.Wait:
-                Wait();
+                Direction();
+                if(!_iswait)//コルーチンの重複防ぐ
+                    StartCoroutine(Wait());//しばらく待ってMoveに
                 break;
 
             case EnemyState.Attack:
-                Attack();
-                _state = EnemyState.Move;
+                if(!_isattack)
+                    StartCoroutine(Attack());
+                _state = EnemyState.Wait;
                 break;
         }
 
+        if (!_isGrounded)
+        {
+            _fallTime += Time.deltaTime;
 
+            _fallSpeed = Physics.gravity.y * _fallTime * 5f * 5f; //Unityの標準重力に任せたいなら fallSpeed は不要
+
+            velocity.y += _fallSpeed * Time.fixedDeltaTime;
+        }
+        else
+        {
+            //velocity.x = 0f;
+        }
+            _rb.velocity = velocity;
     }
 
     private void Look()
     {
-
+        _direction = 0;
     }
 
-    private void Move()
+    private void Direction()
     {
-        Vector3 velocity = _rb.velocity;
+        if(!_isGrounded)
+        {
+            return;
+        }
 
+        Vector3 velocity = _rb.velocity;
 
         if (_moveStop)
         {
@@ -100,9 +132,6 @@ public class DogEnemy : MonoBehaviour
             _direction = -1;
         }
 
-        //FrontJump();
-        //BackJump();
-
         _rb.velocity = velocity;
     }
 
@@ -118,13 +147,77 @@ public class DogEnemy : MonoBehaviour
         yield break;
     }
 
-    private void Wait()
+    private IEnumerator Move()
     {
+        _ismove = true;
+        Vector3 velocity = _rb.velocity;
+        int _rand = Random.Range(1, 4);
+        if (Vector3.Distance(transform.position, _player.position) > 7f)
+        {
+            Debug.Log("backjump");
+            _rb.AddForce(_direction * 13f, _jumpPower, 0f, ForceMode.Impulse);
+            if (_isGrounded)
+            {
+                velocity.x = 0f;//着地で止まる（意味ないかも
+                _state = EnemyState.Attack;
+            }
+            
+        }
+        else if(Vector3.Distance(transform.position, _player.position) <= 7f &&
+            Vector3.Distance(transform.position, _player.position) > 3f)
+        {
+            _state = EnemyState.Attack;
+        }
+        else
+        {
+            Debug.Log("frontjump");
+            _rb.AddForce(_direction * -9f, _jumpPower, 0f, ForceMode.Impulse);
+            if (_isGrounded)
+            {
+                velocity.x = 0f;//着地で止まる（意味ないかも
+                _state = EnemyState.Attack;
+            }
+        }
 
+        _rb.velocity = velocity;
+        _ismove = false;
+        yield break;
     }
 
-    private void Attack()
+    private IEnumerator Wait()
     {
+        _iswait = true;
+        yield return new WaitForSeconds(2f);
+        _state = EnemyState.Move;
+        _iswait = false;
+        yield break;
+    }
 
+    private IEnumerator Attack()
+    {
+        _isattack = true;
+        yield return new WaitForSeconds(0.5f);
+
+        Debug.Log("Attack");
+        _isattack = false;
+        yield break;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Grounded"))
+        {
+            _fallTime = 0f;
+            _isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Grounded"))
+        {
+            _fallTime = 0f;
+            _isGrounded = false;
+        }
     }
 }
