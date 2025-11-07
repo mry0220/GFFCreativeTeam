f130406d1a8690937ce0db89933c97c4a749de18
sho-a4649 <kmm2559347@stu.o-hara.ac.jp>
2025-11-07 15:12:39 +0900

    UI
    フェードイン、フェードアウト、クールタイム

--- MechaAction/Assets/yamamoto/Script/CountdownUI.cs
+++ MechaAction/Assets/yamamoto/Script/CountdownUI.cs
@@ -6,16 +6,44 @@ using UnityEngine.UI;
 
 public class CountdownUI : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Text TimerUI;
+
+    public GameObject CircularUI;
+    public Image CooldownImage;
+
+    private float cooldownTime = 3f;
+    private float timer = 0f;
+    private bool isCooling = false;
+
+    private void Start()
     {
-        
+        CircularUI.SetActive(false);
+        CooldownImage.fillAmount = 0f;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R) && !isCooling)
+        {
+            CircularUI.SetActive(true);
+            timer = cooldownTime;
+            isCooling = true;
+            CooldownImage.fillAmount = 1f;
+        }
+
+        if (isCooling)
+        {
+            timer -= Time.deltaTime;
+            CooldownImage.fillAmount = timer / cooldownTime;
+
+            if (timer <= 0f)
+            {
+                isCooling = false;
+                CircularUI.SetActive(false);
+                CooldownImage.fillAmount = 0f;
+            }
+        }
+        TimerUI.GetComponent<Text>().text = timer.ToString("F1");
     }
 }
