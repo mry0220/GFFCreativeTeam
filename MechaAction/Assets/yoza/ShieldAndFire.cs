13341410daaf5ab2da1c1f65f4674ff7c5d39c81
syuu375 <kmm2559313@stu.o-hara.ac.jp>
2025-11-28 11:38:14 +0900

    確認

--- MechaAction/Assets/yoza/ShieldAndFire.cs
+++ MechaAction/Assets/yoza/ShieldAndFire.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using System.Runtime.InteropServices;
+using TMPro;
 using TMPro.EditorUtilities;
 using UnityEngine;
 
 public class ShieldAndFire : MonoBehaviour
-{  
-    
+{
+
     const float ATTACKTIME = 3f;
     const float SHIELDTIME = 3f;
+    const float GRAVITY = -9.81f;
 
     private enum EnemyState
     {
@@ -18,7 +21,7 @@ public class ShieldAndFire : MonoBehaviour
         SHIELD,
         DESTROY,
     };
- 
+
     private Rigidbody _rb;
     private Transform _playerTransfrom;
     private bool _ATTACK;
@@ -27,92 +30,176 @@ public class ShieldAndFire : MonoBehaviour
     private int _dir;
     private int tmp;
     private float _moveSpeed = 5f;
+    private float _gravityTime;
+    private bool _isGround;
     [SerializeField] Vector3 _velocity;
-    
+    private float _attackTime;
+    private float _shieldTime;
+    [SerializeField] private GameObject FlameThrower;
+    [SerializeField] private GameObject _player;
+    private GameObject _myFlameThrower;
+
+
+
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
         _playerTransfrom = GameObject.FindWithTag("Player").transform;
+
     }
 
     private void Start()
     {
-        state =EnemyState.DITECTION;
+        state = EnemyState.DITECTION;
         _velocity = _rb.velocity;
+        _gravityTime = 0;
     }
 
     private void FixedUpdate()
     {
         _distance = Vector3.Distance(_playerTransfrom.position, _rb.position);
-        switch(state)
+
+        if (!(_isGround))
         {
-            case EnemyState.DITECTION:
-                Ditection();
-                break;
+            _gravityTime += Time.deltaTime;
+            _velocity.y = GRAVITY * _gravityTime;
 
-            case EnemyState.MOVE:
-                
-                break;
-                
-            case EnemyState.ATTACK:
+        }
+        else
+        {
+            _gravityTime = 0;
+            _velocity.y = 0;
 
-                break;
+            switch (state)
+            {
+                case EnemyState.DITECTION:
+                    Attack();
+                   // Ditection();
+                    break;
 
-            case EnemyState.SHIELD:
+                case EnemyState.MOVE:
 
-                break;
+                    break;
 
-            case EnemyState.DESTROY:
-                break;
+                case EnemyState.ATTACK:
+                 //   Attack();
+                    state = EnemyState.SHIELD;
+                    break;
+
+                case EnemyState.SHIELD:
+                    Shield();
+                    break;
+
+                case EnemyState.DESTROY:
+                    break;
+            }
         }
     }
 
     private void Ditection()
     {
         if (_distance < 20)
-            //state =
+        {
             Move();
+        }
+        else
+        {
+            _attackTime = 0;
+            _shieldTime = 0;
+        }
     }
 
     private void Move()
     {
 
-       tmp = _dir;
-   
-       _dir = (_rb.position.x < _playerTransfrom.position.x) ? 1 : -1;
-      if (tmp != _dir)
-      {
-           _velocity.x = 0;
-           return;
-       }
-       else
-       {
-           _velocity.x = _dir * _moveSpeed;
-       }
-       
-       if(_distance<4f)
-       {
-           _velocity.x = 0;
-        
-       }
-       
-        _rb.velocity = _velocity;
- 
-    }
+        tmp = _dir;
 
+        _attackTime += Time.deltaTime;
+        //_shieldTime += Time.deltaTime;
+
+        _dir = (_rb.position.x < _playerTransfrom.position.x) ? 1 : -1;
+        if (tmp != _dir)
+        {
+            _velocity.x = 0;
+            return;
+        }
+        else
+        {
+            _velocity.x = _dir * _moveSpeed;
+        }
+
+        if (_distance < 4f)
+        {
+            _velocity.x = 0;
+
+        }
+
+        _rb.velocity = _velocity;
+
+        if (_distance < 4f)
+        {
+           
+            if (_attackTime >= ATTACKTIME)
+            {
+                _attackTime = 0f;
+                state = EnemyState.ATTACK;
+            }
+           
+        }
+
+    }
+    [SerializeField]private float testtime;
+        float Attacktime = 0;
     private void Attack()
     {
-
-       
+        
+        
+        while(Attacktime < 3f)
+        {
+            Attacktime += Time.deltaTime;
+            Debug.Log(Attacktime);
+            Debug.Log("FlameThrower");
+            testtime = Attacktime;
+        }
+        return;
+        //_myFlameThrower = Instantiate(FlameThrower, transform.position, transform.rotation);
+       // if (_shieldTime >= SHIELDTIME)
+       // {
+       //     _shieldTime = 0f;
+       //     Shield();
+       // }
     }
+
+
     private void Shield()
     {
-
+        for(float shieldTime = 0;shieldTime <= 3f; shieldTime += Time.deltaTime)
+            Debug.Log("SHIELD");
+        return;
+           
     }
 
     private void Destroy()
     {
 
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Grounded"))
+        {
+            _isGround = true;
+            Debug.Log("isGround");
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Grounded"))
+        {
+            _isGround = false;
+        }
+    }
+
+    
 }
